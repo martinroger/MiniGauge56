c7dc576098f8ddf6f7507111698af767f6b917dc
Martin Roger <martin.roger@me.com>
2025-12-26 11:13:25 +0100

    CST9127 bad ack

--- main/main.cpp
+++ main/main.cpp
@@ -2,7 +2,18 @@
 #include "esp32_s3_touch_amoled_1_75.h"
 #include "ui.h"
 
+lv_display_t *main_display;
+
 extern "C" void app_main(void)
 {
+    main_display = bsp_display_start();
+    bsp_display_lock(1);
+    ui_init();
+    bsp_display_unlock();
+
+    while(true)
+    {
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
 
 }
