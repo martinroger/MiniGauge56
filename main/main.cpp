2607ba2bf2a6dbba9d30459fe3d790c11aa4c642
Martin Roger <martin.roger@me.com>
2025-12-31 20:03:50 +0100

    Update to bsp 2.0.0

--- main/main.cpp
+++ main/main.cpp
@@ -8,7 +8,14 @@ lv_display_t *main_display;
 extern "C" void app_main(void)
 {
     main_display = bsp_display_start();
-    bsp_display_lock(1);
+    bsp_display_brightness_set(100);
+    // if(bsp_display_lock(100))
+    // {
+    //     ESP_LOGI(__func__,"first mutex taken");
+        
+    //     bsp_display_unlock();
+    // }
+    bsp_display_lock(-1);
     ui_init();
     bsp_display_unlock();
 
@@ -18,17 +25,30 @@ extern "C" void app_main(void)
     ESP_LOGI("TAG", "Test");
     while (true)
     {
-        ESP_LOGI("Pre", "Pre Delay");
+
         vTaskDelay(pdMS_TO_TICKS(100));
         brightness += 1;
         brightness %= 100;
-        if (lvgl_port_lock(1))
+        
+        if (!bsp_display_lock(-1))
         {
-            if (bsp_display_brightness_set(brightness) != ESP_OK)
-                ESP_LOGE(__func__, "Cannot set brightness");
-            else
-                ESP_LOGI(__func__, "Brightness set to %u", brightness);
-            lvgl_port_unlock();
+            bsp_display_brightness_set(brightness);
+            bsp_display_unlock();
         }
+
+        
+        // bsp_display_brightness_set(brightness);
+        // if (bsp_display_lock(1000))
+        // {
+        //     if (bsp_display_brightness_set(brightness) != ESP_OK)
+        //         ESP_LOGE(__func__, "Cannot set brightness");
+        //     else
+        //         ESP_LOGI(__func__, "Brightness set to %u", brightness);
+        //     bsp_display_unlock();
+        // }
+        // else
+        // {
+        //     ESP_LOGW(__func__,"mutex not available");
+        // }
     }
 }
